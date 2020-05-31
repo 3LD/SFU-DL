465c8afe23f89adcd864cc7c20ade38ff2bb736e
3LD <nickscheel@gmail.com>
2020-05-31 12:31:51 -0700

    remove junk comments

--- sfu_dl/__main__.py
+++ sfu_dl/__main__.py
@@ -4,7 +4,6 @@ import sfu_dl.youtube_dl as youtube_dl
 import os
 import sys
 
-
 if __name__ == "__main__":
 	temp_cookie_file = 'sfu_dl/temp_cookies'
 	open(temp_cookie_file, 'w').close()
@@ -13,12 +12,7 @@ if __name__ == "__main__":
 	for cookie in cookie_jar_chrome:
 		cookie_jar_local.set_cookie(cookie)
 	cookie_jar_local.save(temp_cookie_file, ignore_discard=True, ignore_expires=True)
-	# if (len(sys.argv)>1):
-		# new_args = ["__main__.py", "--config-location", "sfu_dl/config.cfg"]
-		# new_args.append(sys.argv[1]) 
-		# sys.argv = new_args
-		# youtube_dl.main()
-		
+	
 	opts = {
 		'cookiefile': r"sfu_dl/temp_cookies",
 		'outtmpl': r"sfu_dl/output/%(title)s.%(ext)s",
@@ -26,7 +20,6 @@ if __name__ == "__main__":
 		'socket_timeout': 5,
 	}
 	youtube_dl.YoutubeDL(opts).download([sys.argv[1]]);
-		
 
 	os.remove(temp_cookie_file)
 
